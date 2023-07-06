49ffa7d0ab7ef23ad59bff04c1a82067ecb8c7ed
Sahana S <106258347+Sahanasnarayan@users.noreply.github.com>
2023-07-06 18:07:36 +0000

    Sync LeetCode submission - Sqrt(x) (python3)

---
+++ problems/sqrt(x)/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0 or x == 1:
+           return x 
+            
+        left = 1
+        right = x // 2
+
+        while left <= right:
+           mid = (left + right) // 2
+
+           if mid * mid == x:
+              return mid
+
+           if mid * mid < x:
+              left = mid + 1
+              result = mid
+           else:
+              right = mid - 1
+
+        return result
+        
