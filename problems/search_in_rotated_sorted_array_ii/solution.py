ed71ea1bac199304487f889b40290e838c108238
Sahana S <106258347+Sahanasnarayan@users.noreply.github.com>
2023-08-10 17:57:11 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array II (python3)

---
+++ problems/search_in_rotated_sorted_array_ii/solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        
+        left, right = 0, len(nums) - 1
+    
+        while left <= right:
+            mid = (left + right) // 2
+        
+            if nums[mid] == target:
+                return True
+            # Handle duplicates
+            while left < mid and nums[left] == nums[mid]:
+                left += 1
+            # If left half is sorted
+            if nums[left] <= nums[mid]:
+            # Check if target is in the sorted left half
+                if nums[left] <= target <= nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        # If right half is sorted
+            elif nums[mid] <= nums[right]:
+            # Check if target is in the sorted right half
+                if nums[mid] <= target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+    
+        return False      
+        
+        
