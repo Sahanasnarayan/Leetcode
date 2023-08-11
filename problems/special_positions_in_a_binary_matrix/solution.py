e7fed7a9ee1ba795ce70d39b1afedd53bd44c82a
Sahana S <106258347+Sahanasnarayan@users.noreply.github.com>
2023-08-11 18:02:39 +0000

    Sync LeetCode submission - Special Positions in a Binary Matrix (python3)

---
+++ problems/special_positions_in_a_binary_matrix/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numSpecial(self, mat: List[List[int]]) -> int:
+        m, n = len(mat), len(mat[0])
+        row_sum = [0] * m
+        col_sum = [0] * n
+        special_count = 0
+        
+        # Calculate the sum of each row and column
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 1:
+                    row_sum[i] += 1
+                    col_sum[j] += 1
+    
+        # Check for special positions
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 1 and row_sum[i] == 1 and col_sum[j] == 1:
+                    special_count += 1
+    
+        return special_count
