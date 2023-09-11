9d898f72e18d6795b48b290783916ace57b27ae6
Sahana S <106258347+Sahanasnarayan@users.noreply.github.com>
2023-09-11 09:55:13 +0000

    Sync LeetCode submission - Group the People Given the Group Size They Belong To (python)

---
+++ problems/group_the_people_given_the_group_size_they_belong_to/solution.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def groupThePeople(self, groupSizes):
+        count = collections.defaultdict(list)
+        for i, size in enumerate(groupSizes):
+            count[size].append(i)
+        return [l[i:i + s]for s, l in count.items() for i in xrange(0, len(l), s)]
