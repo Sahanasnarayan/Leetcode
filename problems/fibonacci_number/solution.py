f2dfabddc4983ee1dc20b75e20242a175f2a9733
Sahana S <106258347+Sahanasnarayan@users.noreply.github.com>
2023-09-16 07:57:04 +0000

    Sync LeetCode submission - Fibonacci Number (python3)

--- problems/fibonacci_number/solution.py
+++ problems/fibonacci_number/solution.py
@@ -4,12 +4,22 @@ class Solution:
             return 0
         elif n == 1:
             return 1
+        else:
+            return self.recursive_fib(n)
 
-        j, k = 0, 1
-        for i in range(2, n + 1):
-            sums = k + j
-            j = k
-            k = sums
-        return sums  
+    def recursive_fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return self.recursive_fib(n - 1) + self.recursive_fib(n - 2)
+# if i use this code without using cache, u can notice that the computing time and process is too high. so u can use cache as well
+
+
+
+
+
+
 
 
