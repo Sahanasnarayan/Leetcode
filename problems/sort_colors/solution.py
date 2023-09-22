964f88961fb8dd35ada99d7c7652f86651f2f400
Sahana S <106258347+Sahanasnarayan@users.noreply.github.com>
2023-09-22 18:40:13 +0000

    Sync LeetCode submission - Sort Colors (python3)

--- problems/sort_colors/solution.py
+++ problems/sort_colors/solution.py
@@ -1,25 +1,14 @@
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
-       i=j=k=ans=ans1=ans2=0
-       while(i<len(nums)):
-          if( nums[i]==0):
-            ans=ans+1
-            i+=1
-          elif(nums[i]==1):
-            ans1=ans1+1
-            i+=1
-          elif(nums[i]==2):
-            ans2=ans2+1
-            i+=1
-       i=0        
-       while(i<ans):
-          nums[i]=0
-          i+=1
-    
-       while(j<ans1):
-          nums[i+j]=1
-          j+=1
-       j=j+i    
-       while(k<ans2):
-          nums[k+j]=2   
-          k+=1
+class Solution(object):
+    def sortColors(self, nums):
+        low, mid, high = 0, 0, len(nums)-1
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
+
