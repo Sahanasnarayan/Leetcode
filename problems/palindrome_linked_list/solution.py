6d5158617975e59e302c778273eefc078de7d44c
Sahana S <106258347+Sahanasnarayan@users.noreply.github.com>
2023-09-25 18:14:34 +0000

    Sync LeetCode submission - Palindrome Linked List (python3)

---
+++ problems/palindrome_linked_list/solution.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return True
+        slow = fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        second_half = self.reverseList(slow.next)
+    
+        first_half = head
+        while second_half:
+            if first_half.val != second_half.val:
+                return False
+            first_half = first_half.next
+            second_half = second_half.next
+        return True
+    
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        return prev
+
+            
