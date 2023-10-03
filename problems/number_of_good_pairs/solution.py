0b937c5a395ad94f12373887fb2f08b120988e92
Sahana S <106258347+Sahanasnarayan@users.noreply.github.com>
2023-10-03 09:03:18 +0000

    Sync LeetCode submission - Number of Good Pairs (python3)

--- problems/number_of_good_pairs/solution.py
+++ problems/number_of_good_pairs/solution.py
@@ -1,14 +1,17 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         repeat = {}
-        num = 0
-        for v in nums:
-            if v in repeat:
-                if repeat[v] == 1:
-                    num += 1
+        count = 0
+        for value in nums:
+            # if value is in hashmap
+            if value in repeat:
+                if repeat[value] == 1:
+                    count += 1
+                    # if value is in hashmap and its count being more than 2
                 else:
-                    num += repeat[v]
-                repeat[v] += 1
+                    count += repeat[value]
+                repeat[value] += 1
+                # if value is not in hashmap , just add it to hashmap
             else:
-                repeat[v] = 1
-        return num
+                repeat[value] = 1
+        return count
